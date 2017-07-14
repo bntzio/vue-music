85f8d22cb46b1b5e5676093cf7c2009d2c3f010a
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-07-13 19:05:23 -0500

    Render tracks

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,50 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 Vue Music
-    p {{ msg }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text" placeholder="Search songs" v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Search
+          a.button.is-danger.is-large &times;
+      .container.results
+        p
+          small {{ searchMessage }}
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'The Red', artist: 'Chevelle' },
+  { name: '21 Questions', artist: '50 Cent' },
+  { name: 'Cliffs of Dover', artist: 'Eric Johnson' }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  methods: {
+    search () {
+      this.tracks = tracks
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `We found: ${this.tracks.length} songs!`
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import './scss/main.scss'
+@import './scss/main.scss';
+
+.results {
+  margin-top: 25px;
+}
 </style>
